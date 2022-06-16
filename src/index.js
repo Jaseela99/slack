fd850094c64bb8360149269c063b773cfb4f9bc2
Jaseela99 <jaseelachittur@gmail.com>
2022-06-16 15:43:03 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-const container = document.getElementById('root');
-const root = createRoot(container)
 
 root.render(
   <BrowserRouter>
